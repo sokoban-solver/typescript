15acc3fdb82dccd79db70b2375ead0fc5bf90934
Juyeong Maing <mjy9088@naver.com>
2024-04-04 15:04:35 +0900

    improve invalid state detection

--- src/lib.ts
+++ src/lib.ts
@@ -99,17 +99,23 @@ export async function solve(
   onStep?: (state: SokobanState) => Awaitable<unknown>,
   onDistance?: (distance: number) => Awaitable<unknown>
 ): Promise<string | null> {
-  function serialize({ player, boxes }: SokobanState): string {
-    return [player, ...boxes].map(([x, y]) => `${x},${y}`).join("/");
+  function serializePoint([x, y]: Point) {
+    return `${x},${y}`;
   }
 
-  function deserialize(input: string): SokobanState {
-    const [player, ...boxes] = input.split("/").map<Point>((point) => {
-      const tmp = point.split(",");
-      return [parseInt(tmp[0]!), parseInt(tmp[1]!)];
-    });
+  function deserializePoint(input: string): Point {
+    const tmp = input.split(",");
+    return [parseInt(tmp[0]!), parseInt(tmp[1]!)];
+  }
+
+  function serializeState({ player, boxes }: SokobanState): string {
+    return [player, ...boxes].map((point) => serializePoint(point)).join("/");
+  }
+
+  function deserializeState(input: string): SokobanState {
+    const [player, ...boxes] = input.split("/").map<Point>(deserializePoint);
     boxes.sort(([ax, ay], [bx, by]) => ay - by || ax - bx);
-    const boxSet = new Set(boxes.map(([x, y]) => `${x},${y}`));
+    const boxSet = new Set(boxes.map(([x, y]) => serializePoint([x, y])));
     return { player: player!, boxes, boxSet };
   }
 
@@ -117,20 +123,50 @@ export async function solve(
     for (let y = 1; y < height; y++) {
       for (let x = 1; x < width; x++) {
         if (
-          !(
-            wallSet.has(`${x - 1},${y - 1}`) || boxSet.has(`${x - 1},${y - 1}`)
-          ) ||
-          !(wallSet.has(`${x},${y - 1}`) || boxSet.has(`${x},${y - 1}`)) ||
-          !(wallSet.has(`${x - 1},${y}`) || boxSet.has(`${x - 1},${y}`)) ||
-          !(wallSet.has(`${x},${y}`) || boxSet.has(`${x},${y}`))
+          (wallSet.has(serializePoint([x - 1, y - 1])) ||
+            boxSet.has(serializePoint([x - 1, y - 1]))) &&
+          (wallSet.has(serializePoint([x, y - 1])) ||
+            boxSet.has(serializePoint([x, y - 1]))) &&
+          (wallSet.has(serializePoint([x - 1, y])) ||
+            boxSet.has(serializePoint([x - 1, y]))) &&
+          (wallSet.has(serializePoint([x, y])) ||
+            boxSet.has(serializePoint([x, y]))) &&
+          ((!goalSet.has(serializePoint([x - 1, y - 1])) &&
+            boxSet.has(serializePoint([x - 1, y - 1]))) ||
+            (!goalSet.has(serializePoint([x, y - 1])) &&
+              boxSet.has(serializePoint([x, y - 1]))) ||
+            (!goalSet.has(serializePoint([x - 1, y])) &&
+              boxSet.has(serializePoint([x - 1, y]))) ||
+            (!goalSet.has(serializePoint([x, y])) &&
+              boxSet.has(serializePoint([x, y]))))
         )
-          continue;
+          return true;
         if (
-          (boxSet.has(`${x - 1},${y - 1}`) &&
-            !goalSet.has(`${x - 1},${y - 1}`)) ||
-          (boxSet.has(`${1},${y - 1}`) && !goalSet.has(`${1},${y - 1}`)) ||
-          (boxSet.has(`${x - 1},${y}`) && !goalSet.has(`${x - 1},${y}`)) ||
-          (boxSet.has(`${x},${y}`) && !goalSet.has(`${x},${y}`))
+          boxSet.has(serializePoint([x - 1, y - 1])) &&
+          !goalSet.has(serializePoint([x - 1, y - 1])) &&
+          wallSet.has(serializePoint([x, y - 1])) &&
+          wallSet.has(serializePoint([x - 1, y]))
+        )
+          return true;
+        if (
+          boxSet.has(serializePoint([x, y - 1])) &&
+          !goalSet.has(serializePoint([x, y - 1])) &&
+          wallSet.has(serializePoint([x - 1, y - 1])) &&
+          wallSet.has(serializePoint([x, y]))
+        )
+          return true;
+        if (
+          boxSet.has(serializePoint([x - 1, y])) &&
+          !goalSet.has(serializePoint([x - 1, y])) &&
+          wallSet.has(serializePoint([x, y])) &&
+          wallSet.has(serializePoint([x - 1, y - 1]))
+        )
+          return true;
+        if (
+          boxSet.has(serializePoint([x, y])) &&
+          !goalSet.has(serializePoint([x, y])) &&
+          wallSet.has(serializePoint([x - 1, y])) &&
+          wallSet.has(serializePoint([x, y - 1]))
         )
           return true;
       }
@@ -146,16 +182,18 @@ export async function solve(
     const nextY = player[1] + y;
     if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= height)
       return null;
-    if (wallSet.has(`${nextX},${nextY}`)) return null;
-    if (boxSet.has(`${nextX},${nextY}`)) {
+    if (wallSet.has(serializePoint([nextX, nextY]))) return null;
+    if (boxSet.has(serializePoint([nextX, nextY]))) {
       const nextBoxX = nextX + x;
       const nextBoxY = nextY + y;
-      if (wallSet.has(`${nextBoxX},${nextBoxY}`)) return null;
-      if (boxSet.has(`${nextBoxX},${nextBoxY}`)) return null;
+      if (wallSet.has(serializePoint([nextBoxX, nextBoxY]))) return null;
+      if (boxSet.has(serializePoint([nextBoxX, nextBoxY]))) return null;
       const newBoxes = boxes.filter(([x, y]) => x !== nextX || y !== nextY);
       newBoxes.push([nextBoxX, nextBoxY]);
       newBoxes.sort(([ax, ay], [bx, by]) => ay - by || ax - bx);
-      const newBoxSet = new Set(newBoxes.map(([x, y]) => `${x},${y}`));
+      const newBoxSet = new Set(
+        newBoxes.map(([x, y]) => serializePoint([x, y]))
+      );
       return { player: [nextX, nextY], boxes: newBoxes, boxSet: newBoxSet };
     }
     const result: SokobanState = { player: [nextX, nextY], boxes, boxSet };
@@ -164,42 +202,42 @@ export async function solve(
   }
 
   function isComplete({ boxes }: SokobanState) {
-    return boxes.every(([x, y]) => goalSet.has(`${x},${y}`));
+    return boxes.every(([x, y]) => goalSet.has(serializePoint([x, y])));
   }
 
   let distance = 0;
-  const visited = new Map<string, string>([[serialize(initialState), ""]]);
-  let current: string[] = [serialize(initialState)];
+  const visited = new Map<string, string>([[serializeState(initialState), ""]]);
+  let current: string[] = [serializeState(initialState)];
   while (current.length) {
     await onDistance?.(distance);
     const next: string[] = [];
     for (const serializedState of current) {
-      const state = deserialize(serializedState);
+      const state = deserializeState(serializedState);
       const path = visited.get(serializedState);
       await onStep?.(state);
       const w = nextState(state, [0, -1]);
-      if (w && !visited.has(serialize(w))) {
+      if (w && !visited.has(serializeState(w))) {
         if (isComplete(w)) return `${path}W`;
-        visited.set(serialize(w), `${path}W`);
-        next.push(serialize(w));
+        visited.set(serializeState(w), `${path}W`);
+        next.push(serializeState(w));
       }
       const a = nextState(state, [-1, 0]);
-      if (a && !visited.has(serialize(a))) {
+      if (a && !visited.has(serializeState(a))) {
         if (isComplete(a)) return `${path}A`;
-        visited.set(serialize(a), `${path}A`);
-        next.push(serialize(a));
+        visited.set(serializeState(a), `${path}A`);
+        next.push(serializeState(a));
       }
       const s = nextState(state, [0, 1]);
-      if (s && !visited.has(serialize(s))) {
+      if (s && !visited.has(serializeState(s))) {
         if (isComplete(s)) return `${path}S`;
-        visited.set(serialize(s), `${path}S`);
-        next.push(serialize(s));
+        visited.set(serializeState(s), `${path}S`);
+        next.push(serializeState(s));
       }
       const d = nextState(state, [1, 0]);
-      if (d && !visited.has(serialize(d))) {
+      if (d && !visited.has(serializeState(d))) {
         if (isComplete(d)) return `${path}D`;
-        visited.set(serialize(d), `${path}D`);
-        next.push(serialize(d));
+        visited.set(serializeState(d), `${path}D`);
+        next.push(serializeState(d));
       }
     }
     current = next;
